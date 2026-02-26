fc0dd23812b80e5d298dd86e7ccf19002d07daf8
l0ki03 <hmmediamovie@gmail.com>
2026-02-26 20:51:56 +0530

    Update media.go

--- internal/bot/commands/media.go
+++ internal/bot/commands/media.go
@@ -1,9 +1,38 @@
+package commands
+
+import (
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/biisal/fast-stream-bot/config"
+	botutils "github.com/biisal/fast-stream-bot/internal/bot/bot-utils"
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message/markup"
+	"github.com/gotd/td/tg"
+)
+
+type MediaForwardParams struct {
+	Cfg    *config.Config
+	Update *tg.UpdateNewMessage
+	Client *telegram.Client
+}
+
 func (bc *Context) MediaForwarding(params MediaForwardParams) (tg.UpdatesClass, error) {
+
+	// 🔹 Credit Check
 	if params.Cfg.REF {
 		if bc.dbUser.Credit < params.Cfg.MIN_CREDITS_REQUIRED {
+
 			referUrl := botutils.GetReferLink(bc.userInfo.Username, bc.userInfo.ID)
 			now := time.Now()
-			nextMidnight := time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location())
+			nextMidnight := time.Date(
+				now.Year(),
+				now.Month(),
+				now.Day()+1,
+				0, 0, 0, 0,
+				now.Location(),
+			)
 
 			btn := markup.InlineKeyboard(
 				markup.Row(
@@ -12,7 +41,7 @@ func (bc *Context) MediaForwarding(params MediaForwardParams) (tg.UpdatesClass, 
 			)
 
 			msg := fmt.Sprintf(
-				"You're out of credits! 😢\nYou need %d more credits to use this bot.\n\nWait for %s to get new credits or Refer one user to earn %d credits.",
+				"You're out of credits! 😢\n\nYou need %d more credits.\n\nWait %s or refer a user to earn %d credits.",
 				params.Cfg.MIN_CREDITS_REQUIRED-bc.dbUser.Credit,
 				nextMidnight.Sub(now).Round(time.Second).String(),
 				params.Cfg.INCREMENT_CREDITS,
@@ -22,57 +51,85 @@ func (bc *Context) MediaForwarding(params MediaForwardParams) (tg.UpdatesClass, 
 		}
 	}
 
+	// 🔹 Get Media
 	m := params.Update.Message.(*tg.Message)
-	fromPeer := &tg.InputPeerUser{UserID: bc.userInfo.ID, AccessHash: bc.userInfo.AccessHash}
+	fromPeer := &tg.InputPeerUser{
+		UserID:     bc.userInfo.ID,
+		AccessHash: bc.userInfo.AccessHash,
+	}
 
 	file, err := botutils.GetMediaFromMessage(m)
 	if err != nil {
-		slog.Error("Failed to get media from message", "error", err)
+		slog.Error("Failed to get media", "error", err)
 		return nil, err
 	}
 
 	msgHash := botutils.MakeHashByFileInfo(file)
 
-	_, channelInputPeer, err := botutils.GetChannelPeer(params.Client.API(), bc.ctx, params.Cfg.DB_CHANNEL_ID)
+	// 🔹 Forward to DB Channel
+	_, channelInputPeer, err := botutils.GetChannelPeer(
+		params.Client.API(),
+		bc.ctx,
+		params.Cfg.DB_CHANNEL_ID,
+	)
 	if err != nil {
-		slog.Error("Failed to get channel peer", "error", err)
+		slog.Error("Channel peer error", "error", err)
 		return nil, err
 	}
 
-	fUpdate, err := bc.sender.To(channelInputPeer.InputPeer()).ForwardIDs(fromPeer, m.ID).Send(bc.ctx)
+	fUpdate, err := bc.sender.
+		To(channelInputPeer.InputPeer()).
+		ForwardIDs(fromPeer, m.ID).
+		Send(bc.ctx)
+
 	if err != nil {
-		slog.Error("Failed to forward message", "error", err)
+		slog.Error("Forward failed", "error", err)
 		return nil, err
 	}
 
-	messageId := fUpdate.(*tg.Updates).Updates[0].(*tg.UpdateMessageID).ID
+	messageId := fUpdate.(*tg.Updates).
+		Updates[0].
+		(*tg.UpdateMessageID).
+		ID
 
-	// ✅ Generate both links
-	streamLink := fmt.Sprintf("%s/watch/%d?hash=%s", params.Cfg.FQDN, messageId, msgHash)
-	downloadLink := fmt.Sprintf("%s/download/%d?hash=%s", params.Cfg.FQDN, messageId, msgHash)
+	// 🔥 Generate Links
+	streamLink := fmt.Sprintf(
+		"%s/watch/%d?hash=%s",
+		params.Cfg.FQDN,
+		messageId,
+		msgHash,
+	)
 
-	fileMsg := fmt.Sprintf(
-		"File Name: %s\nFile Size: %s",
+	downloadLink := fmt.Sprintf(
+		"%s/download/%d?hash=%s",
+		params.Cfg.FQDN,
+		messageId,
+		msgHash,
+	)
+
+	// 🔹 Deduct Credit
+	bc.dbUser, err = bc.userService.DecrementCredits(
+		bc.ctx,
+		bc.userInfo.ID,
+		params.Cfg.DECREMENT_CREDITS,
+	)
+	if err != nil {
+		slog.Error("Credit decrement failed", "error", err)
+		return nil, err
+	}
+
+	// 🔹 User Message
+	msg := fmt.Sprintf(
+		"🎉 Your file is ready!\n\n📂 Name: %s\n📦 Size: %s\n\nChoose below:",
 		file.FileName,
 		botutils.MakeSizeReadable(file.Size),
 	)
 
-	bc.dbUser, err = bc.userService.DecrementCredits(bc.ctx, bc.userInfo.ID, params.Cfg.DECREMENT_CREDITS)
-	if err != nil {
-		slog.Error("Failed to decrement credit", "error", err)
-		return nil, err
-	}
-
-	msg := fmt.Sprintf(
-		"Your file is ready! 🎉\n\n%s\n\nChoose an option below:",
-		fileMsg,
-	)
-
 	if params.Cfg.REF {
-		msg += fmt.Sprintf("\n\nYou have %d credits remaining 😊", bc.dbUser.Credit)
+		msg += fmt.Sprintf("\n\n💳 Credits left: %d", bc.dbUser.Credit)
 	}
 
-	// ✅ 2 Buttons
+	// 🔥 TWO BUTTONS
 	btn := markup.InlineKeyboard(
 		markup.Row(
 			markup.URL("▶ Watch Now", streamLink),
@@ -81,36 +138,46 @@ func (bc *Context) MediaForwarding(params MediaForwardParams) (tg.UpdatesClass, 
 	)
 
 	if _, err = bc.builder.Markup(btn).Text(bc.ctx, msg); err != nil {
-		slog.Error("Failed to send message", "error", err)
 
-		if _, err := bc.userService.IncrementCredits(bc.ctx, bc.userInfo.ID, params.Cfg.DECREMENT_CREDITS, false); err != nil {
-			slog.Error("Failed to increment credit", "error", err)
-		}
+		slog.Error("Send message failed", "error", err)
+
+		// rollback credit
+		_, _ = bc.userService.IncrementCredits(
+			bc.ctx,
+			bc.userInfo.ID,
+			params.Cfg.DECREMENT_CREDITS,
+			false,
+		)
 
 		return nil, err
 	}
 
-	if bc.dbUser, err = bc.userService.IncrementTotalLinkCount(bc.ctx, bc.dbUser.ID); err != nil {
-		slog.Error("Failed to update user", "error", err)
+	// 🔹 Update total links
+	bc.dbUser, _ = bc.userService.
+		IncrementTotalLinkCount(bc.ctx, bc.dbUser.ID)
+
+	// 🔹 Delete user original message
+	if bc.userInfo.ID != params.Cfg.ADMIN_ID {
+		_, _ = params.Client.API().
+			MessagesDeleteMessages(bc.ctx,
+				&tg.MessagesDeleteMessagesRequest{
+					Revoke: true,
+					ID:     []int{m.ID},
+				},
+			)
 	}
 
-	// Log message
-	fileMsg = fmt.Sprintf("User: %s\nUserId: %d\n\nFile Name: %s\nFile Size: %s",
+	// 🔹 Log in DB channel
+	logMsg := fmt.Sprintf(
+		"User: %s\nUserID: %d\n\nFile: %s\nSize: %s",
 		bc.userInfo.Username,
 		bc.userInfo.ID,
 		file.FileName,
 		botutils.MakeSizeReadable(file.Size),
 	)
 
-	if bc.userInfo.ID != params.Cfg.ADMIN_ID {
-		_, err = params.Client.API().MessagesDeleteMessages(bc.ctx, &tg.MessagesDeleteMessagesRequest{
-			Revoke: true,
-			ID:     []int{m.ID},
-		})
-		if err != nil {
-			slog.Error("Failed to delete user message", "error", err)
-		}
-	}
-
-	return bc.sender.To(channelInputPeer.InputPeer()).Reply(messageId).Text(bc.ctx, fileMsg)
+	return bc.sender.
+		To(channelInputPeer.InputPeer()).
+		Reply(messageId).
+		Text(bc.ctx, logMsg)
 }
